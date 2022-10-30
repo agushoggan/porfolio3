534fba6797242c5d8a7a6bc168d3eb384ecf7859
agustin <agushogggan@gmail.com>
2022-10-30 18:38:31 -0300

    porfolio3

---
+++ src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component, ElementRef, ViewChild,OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+
+export class AppComponent {
+  title = 'proyecto_x';
+  @ViewChild('seccionAbout') secFotosuno: ElementRef | any;
+  @ViewChild('seccionExperiencia') secFotosdos: ElementRef | any;
+  @ViewChild('seccionSkill') secFotostres: ElementRef | any;
+  @ViewChild('seccionEducation') secFotoscuatro: ElementRef | any;
+  @ViewChild('seccionContacto') secFotoscinco: ElementRef | any;
+  scrollUno() {
+    this.secFotosuno.nativeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
+    });
+  }
+  scrollDos() {
+    this.secFotosdos.nativeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
+    });
+  }
+  scrollTres() {
+    this.secFotostres.nativeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
+    });
+  }
+  scrollCuatro() {
+    this.secFotoscuatro.nativeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
+    });
+  }
+  scrollCinco() {
+    this.secFotoscinco.nativeElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+        inline: 'nearest'
+    });
+  }
+  Instagram(){
+    window.open('https://www.instagram.com/agusshoggan/');
+  }
+  Github(){
+    window.open('https://github.com/agushoggan');
+  }
+  Linkedin(){
+    window.open('https://www.linkedin.com/in/agustin-hoggan-962564228/');
+  }
+  Twitter(){
+    window.open('https://twitter.com/AgustiinHoggan');
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {MatInputModule} from '@angular/material/input';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { NavComponent } from './nav/nav.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
